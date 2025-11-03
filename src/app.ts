d71bfbf586a24bf3f8669e3244c6013347d3e52d
Sao <Vipin.sao@ssipmt.com>
2025-11-03 19:54:34 +0530

    add errorn handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,33 @@
-import express from "express";
+import express, {
+  type NextFunction,
+  type Request,
+  type Response,
+} from "express";
+import logger from "./config/logger.js";
+import type { HttpError } from "http-errors";
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Welcome to Auth Service");
+});
+
+// creating global error handler middleware
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+  logger.error(err.message);
+  const statusCode = err.status || 500;
+
+  res.status(statusCode).json({
+    errors: [
+      {
+        type: err.name,
+        msg: err.message,
+        path: "",
+        location: "",
+      },
+    ],
+  });
+  next(err);
 });
 
 export default app;
